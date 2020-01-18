0b85fec43dfcc420e3cdc99b76a7c607a01e3a57
ll2313673906 <3340624204@qq.com>
2020-01-18 20:09:35 +0800

    知乎-圆桌页面前端页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios,axios)
 
 Vue.config.productionTip = false
+
+
+// 自定义过滤器
+Vue.filter("suolv",function (value) {
+  return value.slice(0,100) + "..."
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,74 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Nav from '../views/Nav.vue'
+import Explore from '../views/Explore.vue'
+import Special from '../views/Special.vue'
+import Recommoned from '../views/Recommoned.vue'
+import Follow from '../views/Follow.vue'
+import Hot from '../views/Hot.vue'
+import QuestionWating from '../views/QuestionWaiting.vue'
+import Login from '../views/Login.vue'
+import ExploreMore from '../views/ExploreMore.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		component:Nav,
+		children:[{
+			path:'/',
+			redirect:'home'
+		},
+			{
+				path:'home',
+				component:() => import('../views/Home.vue'),
+				children:[
+					{
+						path:'/',
+						redirect:'recommoned'
+					},
+					{
+						path:'recommoned',
+						component:()=> import('../views/Recommoned.vue')
+					},
+					{
+						path:'follow',
+						component:() =>import('../views/Follow.vue')
+					},
+					{
+						path:'hot',
+						component:() => import('../views/Hot.vue')
+					}
+				]
+			},
+			{
+				path:'explore',
+				component:() => import('../views/Explore.vue'),
+
+			},
+			{
+				path:'explore-more',
+				component:() => import('../views/ExploreMore.vue'),
+			},
+			{
+				path:'special/all',
+				component: ()=> import('../views/Special.vue')
+			},
+			{
+				path:'question/waiting',
+				component: ()=>import('../views/QuestionWaiting.vue')
+			},
+
+		]
+	},
+	{
+		path:'/login',
+		component: Login
+	}
 ]
-
 const router = new VueRouter({
-  routes
+	routes
 })
-
 export default router
